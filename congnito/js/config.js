9c653ac6b9404e549d3d85d8fce462dd22d36da6
sahuamrita <amrita.sahu1510@gmail.com>
2019-08-15 01:47:52 +0100

    cognito changes succesful implementation

---
+++ congnito/js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_rDg7bL3eU', // e.g. us-east-2_uXboG5pAb
+        region: 'eu-west-1', // e.g. us-east-2
+		clientId: '17o5c4kmscgs9mliga2t75uhc4' //is this used anywhere?
+    },
+};
+
